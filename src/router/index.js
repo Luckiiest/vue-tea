e5cda6de7bb9f431e8f8558de9e2189cc2ed9078
my <15533323693@163.com>
2022-05-30 13:42:15 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,159 +1,169 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
 // 解决路由版本问题
-const originalPush = VueRouter.prototype.push
-const originalReplace = VueRouter.prototype.replace
+const originalPush = VueRouter.prototype.push;
+const originalReplace = VueRouter.prototype.replace;
 // push
 VueRouter.prototype.push = function push(location, onResolve, onReject) {
-  if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject)
-  return originalPush.call(this, location).catch((err) => err)
-}
+  if (onResolve || onReject)
+    return originalPush.call(this, location, onResolve, onReject);
+  return originalPush.call(this, location).catch((err) => err);
+};
 // replace
 VueRouter.prototype.replace = function push(location, onResolve, onReject) {
-  if (onResolve || onReject) return originalReplace.call(this, location, onResolve, onReject)
-  return originalReplace.call(this, location).catch((err) => err)
-}
+  if (onResolve || onReject)
+    return originalReplace.call(this, location, onResolve, onReject);
+  return originalReplace.call(this, location).catch((err) => err);
+};
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    redirect: '/home',
+    path: "/",
+    redirect: "/home",
   },
   {
-    path: '/home',
-    name: 'Home',
+    path: "/home",
+    name: "Home",
     component: Home,
   },
   {
-    path: '/list',
-    name: 'List',
-    component: () => import('../views/List.vue'),
+    path: "/list",
+    name: "List",
+    component: () => import("../views/List.vue"),
   },
   {
-    path: '/cart',
-    name: 'Cart',
-    component: () => import('../views/Cart.vue'),
+    path: "/cart",
+    name: "Cart",
+    component: () => import("../views/Cart.vue"),
   },
   {
-    path: '/my',
-    name: 'My',
-    component: () => import('../views/My.vue'),
+    path: "/my",
+    name: "My",
+    component: () => import("../views/My.vue"),
   },
   {
-    path: '/search',
-    name: 'Search',
+    path: "/search",
+    name: "Search",
     children: [
       {
-        path: '/',
-        name: 'index',
-        component: () => import('../views/search/Search-index.vue'),
+        path: "/",
+        name: "index",
+        component: () => import("../views/search/Search-index.vue"),
       },
       {
-        path: 'list',
-        name: 'list',
-        component: () => import('../views/search/Search-list.vue'),
+        path: "list",
+        name: "list",
+        component: () => import("../views/search/Search-list.vue"),
       },
     ],
-    component: () => import('../views/Search.vue'),
+    component: () => import("../views/Search.vue"),
   },
   {
-    path: '/detail',
-    name: 'Detail',
+    path: "/detail",
+    name: "Detail",
     meta: {
       keepAlive: true,
     },
-    component: () => import('../views/Detail.vue'),
+    component: () => import("../views/Detail.vue"),
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/login/Login.vue'),
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/login/Login.vue"),
   },
   {
-    path: '/userLogin',
-    name: 'UserLogin',
-    component: () => import('../views/login/UserLogin.vue'),
+    path: "/userLogin",
+    name: "UserLogin",
+    component: () => import("../views/login/UserLogin.vue"),
   },
   {
-    path: '/register',
-    name: 'Register',
-    component: () => import('../views/login/Register.vue'),
+    path: "/register",
+    name: "Register",
+    component: () => import("../views/login/Register.vue"),
   },
   {
-    path: '/recovery',
-    name: 'Recovery',
+    path: "/recovery",
+    name: "Recovery",
     children: [
       {
-        path: '/',
-        name: 'RecoveryIndex',
-        component: () => import('../views/recovery/RecoveryIndex.vue'),
+        path: "/",
+        name: "RecoveryIndex",
+        component: () => import("../views/recovery/RecoveryIndex.vue"),
       },
       {
-        path: 'btn',
-        name: 'Btn',
-        component: () => import('../views/recovery/RecoveryBtn.vue'),
+        path: "btn",
+        name: "Btn",
+        component: () => import("../views/recovery/RecoveryBtn.vue"),
       },
     ],
-    component: () => import('../views/recovery/Recovery.vue'),
+    component: () => import("../views/recovery/Recovery.vue"),
   },
   {
-    path: '/path',
-    name: 'Path',
+    path: "/path",
+    name: "Path",
     children: [
       {
-        path: '/',
-        name: 'PathIndex',
-        component: () => import('../views/path/Path-Index.vue'),
+        path: "/",
+        name: "PathIndex",
+        component: () => import("../views/path/Path-Index.vue"),
       },
       {
-        path: '/path-list',
-        name: 'path-list',
-        component: () => import('../views/path/Path-List.vue'),
+        path: "/path-list",
+        name: "PathList",
+        component: () => import("../views/path/Path-List.vue"),
       },
     ],
-    component: () => import('../views/Path.vue'),
+    component: () => import("../views/Path.vue"),
   },
   {
-    path: '/order',
-    name: 'Order',
+    path: "/order",
+    name: "Order",
     meta: {
       keepAlive: true,
     },
-    component: () => import('../views/Order.vue'),
+    component: () => import("../views/Order.vue"),
   },
   {
-    path: '/payment',
-    name: 'Payment',
+    path: "/payment",
+    name: "Payment",
     meta: {
       keepAlive: true,
     },
-    component: () => import('../views/Payment.vue'),
+    component: () => import("../views/Payment.vue"),
   },
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes,
-})
+});
 
 router.beforeEach((to, from, next) => {
-  let nextRoute = ['Payment', 'Cart', 'Path', 'Order', 'Path', 'PathIndex', 'path-list']
+  let nextRoute = [
+    "Payment",
+    "Cart",
+    "Path",
+    "Order",
+    "Path",
+    "PathIndex",
+    "PathList",
+  ];
   //是否是登录中
-  let userInfo = JSON.parse(localStorage.getItem('UserInfo'))
+  let userInfo = JSON.parse(localStorage.getItem("UserInfo"));
 
   //当前进入的页面，是不是需要验证哪些页面
   if (nextRoute.indexOf(to.name) >= 0) {
     if (!userInfo) {
-      router.push('/login')
+      router.push("/login");
     }
   }
 
-  next()
-})
+  next();
+});
 
-export default router
+export default router;
